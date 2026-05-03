0e308383426dbf65fee8f26e7b70da0ce1b68c95
martbungque <raymartbungque18@gmail.com>
2026-05-04 06:20:25 +0800

    feat: update medicine route to search by query with typo tolerance

--- backend/src/routes/medicine.routes.ts
+++ backend/src/routes/medicine.routes.ts
@@ -1,12 +1,11 @@
 import express from 'express';
-import { getMedicines } from '../controllers/medicine.controller.js';
+import { searchMedicine } from '../controllers/medicine.controller.js';
 
 const medicineRouter = express.Router();
 
-// @desc Get all medicines or search by name
-// @route GET api/medicines
-// @route GET /medicines?search=medicineName
-// @access Public
-medicineRouter.get('/', getMedicines);
+// @desc Search medicines by query (with typo tolerance)
+// GET /api/medicines?search=paracetamol
+// GET /api/medicines?search=paracelamot
+medicineRouter.get('/', searchMedicine);
 
 export default medicineRouter;
